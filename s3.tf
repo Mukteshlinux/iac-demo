d5ef12f2ad6c5be5bf39f4851e5465f409b6a273
Mukteshlinux <105842083+Mukteshlinux@users.noreply.github.com>
2022-06-14 12:34:21 +0530

    se3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,13 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "muktesh705"
+  bucket = "muktesh03"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
 }
